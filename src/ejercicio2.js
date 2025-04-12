8b788adce6814abf0d17a2cab4309c109999da01
JuanCarrizo9 <juan.seb.car09@gmail.com>
2025-04-12 02:35:21 -0300

    Ejercicio 1, 2 y 3 resueltos por Juan Carrizo

---
+++ src/ejercicio2.js
@@ -0,0 +1,10 @@
+console.log("Los primeros 10 números pares son: ")
+
+let i = 2;
+let count = 0;
+
+while (count < 10){
+    console.log(i);
+    i += 2;
+    count++;
+}
---
+++ src/ejercicio3.js
@@ -0,0 +1,6 @@
+let radio = 7.5;
+const pi = Math.PI;
+
+let area = pi * radio **2;
+
+console.log(area)
